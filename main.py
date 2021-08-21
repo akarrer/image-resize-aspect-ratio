c36c0fcb3be72438b7f78966be7c08a3e32a15ac
akarrer <32549276+akarrer@users.noreply.github.com>
2021-08-21 13:31:34 -0600

    Update main.py

--- main.py
+++ main.py
@@ -5,18 +5,18 @@ import PIL.Image
 from PIL import Image, ImageFilter
 import math
 
-def resizeImage(numPixels, imagePath):
+def resize_image(num_pixels, image_path):
     with Image.open(imagePath) as img:
-        currentImageHeight = img.height
-        currentImageWidth = img.width
-        aspectRatio = currentImageWidth / float(currentImageHeight)
-        newWidth = math.floor(math.sqrt(numPixels / aspectRatio))
-        newHeight = math.floor(aspectRatio * newWidth)
-        newImage = img.resize((newHeight, newWidth), resample=PIL.Image.LANCZOS)
-        newImage.save("parrotLANCZOS.jpg", quality=95)
-        imgSharpened = newImage.filter(ImageFilter.SHARPEN)
-        imgSharpened.save("parrotsharpened2.jpg")
+        current_image_height = img.height
+        current_image_width = img.width
+        aspect_ratio = current_image_width / float(current_image_height)
+        new_width = math.floor(math.sqrt(num_pixels / aspect_ratio))
+        new_height = math.floor(aspect_ratio * new_width)
+        new_image = img.resize((new_height, new_width), resample=PIL.Image.LANCZOS)
+        new_image.save("parrotLANCZOS.jpg", quality=95)
+        img_sharpened = new_image.filter(ImageFilter.SHARPEN)
+        img_sharpened.save("parrotsharpened2.jpg")
 
 desktop = os.path.join(os.path.join(os.environ['USERPROFILE']), "Desktop")
-fullFileName = os.path.join(desktop, "parrot.jpg")
-resizeImage(2814, fullFileName)
+full_file_name = os.path.join(desktop, "parrot.jpg")
+resize_image(2814, full_file_name)
